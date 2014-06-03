9a46d1c42bcab2cd68cc1c0d1f0e88bfa26a1653
Alexei Rayu <alex.rayu@gmail.com>
2014-06-03 11:06:46 +0300

    Converted module to PSR-4.

--- js/sample5.js
+++ js/sample5.js
@@ -12,9 +12,7 @@ var s5_app = angular.module('s5_app', []);
  * Declare the controller that adds the default value to scope var.
  */
 s5_app.controller('s5_ctrl', function s5_ctrl($scope) {
-  $scope.s5setCommon = function() {
-    jQuery('span.s5 input').val('Initial text');
-  }
+
 });
 
 /**
@@ -28,8 +26,21 @@ s5_app.directive('s5', function() {
     },
     link: function($scope, $elem, $attrs) {
       $scope.s5model = $scope.initext;
+
     },
-    template: '<input type="text" ng-model="s5model" /> {{s5model}}'
+    template: '<input type="text" ng-model="s5model" /> {{s5model}}',
+  }
+});
+
+/**
+ * A directive to bind event to element, creating a behavior.
+ */
+s5_app.directive('s5button', function() {
+  return {
+    restrict: 'C',
+    link: function($scope, $elem, $attrs) {
+      alert($scope.s5model);
+    },
   }
 });
 
